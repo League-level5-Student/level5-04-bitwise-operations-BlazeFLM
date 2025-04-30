f8c5eeae95b0d66124c83c3d7aa62281128abd62
blazi <blazi@DESKTOP-9JBQAR7>
2025-04-29 18:27:48 -0700

    6:27 PM 4/29/2025

--- src/_03_Printing_Binary/_01_BinaryPrinter.java
+++ src/_03_Printing_Binary/_01_BinaryPrinter.java
@@ -9,20 +9,20 @@ public class _01_BinaryPrinter {
 
 	public static void printByteBinary(byte b) {
 		// We first want to print the bit in the one's place
-		
+
 		// Shift b seven bits to the right
-		
+
 		// Use the & operator to "mask" the bit in the one's place
 		// This can be done by "anding" (&) it with the value of 1
-		
+
 		// Print the result using System.out.print (NOT System.out.println)
-		
+
 		// Use this method to print the remaining 7 bits of b
-		//01010101
+		// 01010101
 		String bin = "";
 		do {
 			b >>>= 1;
-			if(b % 2 == 0) {
+			if (b % 2 == 0) {
 				bin += "0";
 			} else {
 				bin += "1";
@@ -31,15 +31,19 @@ public class _01_BinaryPrinter {
 		System.out.println(bin);
 	}
 
-	public void printShortBinary(short s) {
+	public static void printShortBinary(short s) {
 		// Create 2 byte variables
-
+		// byte a = 0b00000000;
+		// byte b = 0b00000000;
 		// Use bit shifting and masking (&) to save the first
 		// 8 bits of s in one byte, and the second 8 bits of
 		// s in the other byte
-
+		byte a = (byte) (s & 0b11111111);
+		byte b = (byte) (s >> 8);
 		// Call printByteBinary twice using the two bytes
 		// Make sure they are in the correct order
+		printByteBinary(b);
+		printByteBinary(a);
 	}
 
 	public void printIntBinary(int i) {
@@ -60,5 +64,7 @@ public class _01_BinaryPrinter {
 	public static void main(String[] args) {
 		// Test your methods here
 		printByteBinary((byte) 01010101);
+		printShortBinary((short) 0b0011001010101010);
+
 	}
 }
